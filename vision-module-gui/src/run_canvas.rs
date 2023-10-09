ff781c14c38276a0655946d21c877f49d8e15f29
Abraham Hamidi <abraham.hamidi01@gmail.com>
2023-10-09 16:38:23 -0500

    Crosshairs in boundaries

--- vision-module-gui/src/run_canvas.rs
+++ vision-module-gui/src/run_canvas.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use iui::controls::{Area, AreaDrawParams, AreaHandler};
-use iui::draw::{Brush, FillMode, Path, SolidBrush};
-use iui::UI;
+use iui::draw::{Brush, FillMode, LineCap, LineJoin, Path, SolidBrush, StrokeParams};
+use iui::{draw, UI};
 use crate::MotState;
 
 pub struct RunCanvas {
@@ -14,6 +14,7 @@ impl AreaHandler for RunCanvas {
     fn draw(&mut self, _area: &Area, draw_params: &AreaDrawParams) {
         let ctx = &draw_params.context;
 
+        let ch_path = Path::new(ctx, FillMode::Winding);
         let nf_path = Path::new(ctx, FillMode::Winding);
         let wf_path = Path::new(ctx, FillMode::Winding);
         let state = self.state.blocking_lock();
@@ -23,14 +24,22 @@ impl AreaHandler for RunCanvas {
                     break;
                 }
                 // todo don't use hardcoded 4096x4096 res assumption
-                let x = mot_data.cx as f64 / 4096.;
-                let y = mot_data.cy as f64 / 4096.;
+                let x = mot_data.cx as f64 / 4096. * draw_params.area_width;
+                let y = mot_data.cy as f64 / 4096. * draw_params.area_height;
+
+                let left = mot_data.boundary_left as f64 / 98. * draw_params.area_width;
+                let down = mot_data.boundary_down as f64 / 98. * draw_params.area_height;
+                let right = mot_data.boundary_right as f64 / 98. * draw_params.area_width;
+                let up = mot_data.boundary_up as f64 / 98. * draw_params.area_height;
+
+                draw_crosshair(&ctx, &ch_path, x, y);
+
                 nf_path.add_rectangle(
                     ctx,
-                    x * draw_params.area_width,
-                    y * draw_params.area_height,
-                    draw_params.area_width / 100.,
-                    draw_params.area_width / 100.,
+                    left,
+                    up,
+                    right-left,
+                    down-up,
                 );
             }
         }
@@ -41,24 +50,34 @@ impl AreaHandler for RunCanvas {
                     break;
                 }
                 // todo don't use hardcoded 4096x4096 res assumption
-                let x = mot_data.cx as f64 / 4096.;
-                let y = mot_data.cy as f64 / 4096.;
+                let x = mot_data.cx as f64 / 4096. * draw_params.area_width;
+                let y = mot_data.cy as f64 / 4096. * draw_params.area_height;
+
+                let left = mot_data.boundary_left as f64 / 98. * draw_params.area_width;
+                let down = mot_data.boundary_down as f64 / 98. * draw_params.area_height;
+                let right = mot_data.boundary_right as f64 / 98. * draw_params.area_width;
+                let up = mot_data.boundary_up as f64 / 98. * draw_params.area_height;
+
+                draw_crosshair(&ctx, &ch_path, x, y);
+
                 wf_path.add_rectangle(
                     ctx,
-                    x * draw_params.area_width,
-                    y * draw_params.area_height,
-                    draw_params.area_width / 100.,
-                    draw_params.area_width / 100.,
+                    left,
+                    up,
+                    right-left,
+                    down-up,
                 );
             }
         }
         wf_path.end(ctx);
 
+        ch_path.end(ctx);
+
         let brush = Brush::Solid(SolidBrush {
             r: 1.,
             g: 0.,
             b: 0.,
-            a: 1.,
+            a: 0.5,
         });
 
         draw_params.context.fill(&nf_path, &brush);
@@ -67,9 +86,35 @@ impl AreaHandler for RunCanvas {
             r: 0.,
             g: 0.,
             b: 1.,
-            a: 1.,
+            a: 0.5,
         });
 
         draw_params.context.fill(&wf_path, &brush);
+
+        let brush = Brush::Solid(SolidBrush {
+            r: 0.,
+            g: 0.,
+            b: 0.,
+            a: 1.,
+        });
+
+        let stroke = StrokeParams {
+            cap: 0, // Bevel
+            join: 0, // Flat
+            thickness: 2.,
+            miter_limit: 0.,
+            dashes: vec![],
+            dash_phase: 0.,
+        };
+
+        ctx.stroke(&ch_path, &brush, &stroke);
     }
+}
+
+fn draw_crosshair(ctx: &draw::DrawContext, path: &Path, x: f64, y: f64) {
+    path.new_figure(ctx, x-4., y);
+    path.line_to(ctx, x+4., y);
+
+    path.new_figure(ctx, x, y-4.);
+    path.line_to(ctx, x, y+4.);
 }
