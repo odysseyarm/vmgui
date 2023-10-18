772f41e99e2f3a60ac39e0ce04a6c01ac0b0af76
Abraham Hamidi <abraham.hamidi01@gmail.com>
2023-10-18 14:12:52 -0500

    Add foveated aim point. Completely untested, may god help us

--- vision-module-gui/src/mot_runner.rs
+++ vision-module-gui/src/mot_runner.rs
@@ -39,12 +39,12 @@ fn transform_aim_point_to_identity(center_aim: Point2<f64>, p1: Point2<f64>, p2:
                         Point2::new(0.5, 0.), Point2::new(1., 0.5))
 }
 
-fn sort_clockwise(a: &mut [MotData]) {
-    if a[0].cy < a[1].cy { a.swap(0, 1); }
-    if a[2].cy > a[3].cy { a.swap(2, 3); }
-    if a[0].cy < a[3].cy { a.swap(0, 3); }
-    if a[2].cy > a[1].cy { a.swap(2, 1); }
-    if a[1].cx > a[3].cx { a.swap(1, 3); }
+fn sort_clockwise(a: &mut [Point2<f64>]) {
+    if a[0].y < a[1].y { a.swap(0, 1); }
+    if a[2].y > a[3].y { a.swap(2, 3); }
+    if a[0].y < a[3].y { a.swap(0, 3); }
+    if a[2].y > a[1].y { a.swap(2, 1); }
+    if a[1].x > a[3].x { a.swap(1, 3); }
 }
 
 pub struct MotRunner {
@@ -59,31 +59,82 @@ impl MotRunner {
                 return;
             }
             let device = self.device.c().unwrap();
-            let (nf_data, wf_data) = device.get_frame().await.expect("Problem getting frame from device");
+            if let Ok((nf_data, wf_data)) = device.get_frame().await {
+                let mut nf_data = ArrayVec::<MotData,16>::from_iter(nf_data.into_iter().filter(|x| x.area > 0));
+                let mut wf_data = ArrayVec::<MotData,16>::from_iter(wf_data.into_iter().filter(|x| x.area > 0));
+                
+                let mut nf_points = ArrayVec::<Point2<f64>,16>::from_iter(nf_data.as_ref().into_iter().map(|x| Point2::new(x.cx as f64, x.cy as f64)));
+                let mut wf_points = ArrayVec::<Point2<f64>,16>::from_iter(wf_data.as_ref().into_iter().map(|x| Point2::new(x.cx as f64, x.cy as f64)));
 
-            let mut nf_data = ArrayVec::<MotData,16>::from_iter(nf_data.into_iter().filter(|x| x.area > 0));
-            let mut wf_data = ArrayVec::<MotData,16>::from_iter(wf_data.into_iter().filter(|x| x.area > 0));
+                let mut fv_aim_point = None::<Point2<f64>>;
+                let mut nf_aim_point = None::<Point2<f64>>;
+                let mut wf_aim_point = None::<Point2<f64>>;
 
-            let mut nf_aim_point = None::<Point2<f64>>;
-            let mut wf_aim_point = None::<Point2<f64>>;
+                let center_aim = Point2::new(2048.0, 2048.0);
 
-            for (data, aim_point) in [(&mut nf_data, &mut nf_aim_point), (&mut wf_data, &mut wf_aim_point)] {
-                if data.len() > 3 {
-                    sort_clockwise(&mut data[0..4]);
-                    *aim_point = transform_aim_point_to_identity(Point2::new(2048.0, 2048.0),
-                                                                 Point2::new(data[0].cx as f64, data[0].cy as f64),
-                                                                 Point2::new(data[1].cx as f64, data[1].cy as f64),
-                                                                 Point2::new(data[2].cx as f64, data[2].cy as f64),
-                                                                 Point2::new(data[3].cx as f64, data[3].cy as f64));
-                    if aim_point.is_none() { println!("Linear resolution failed with points: ({},{}), ({},{}), ({},{}), ({},{})", data[0].cx, data[0].cy, data[1].cx, data[1].cy, data[2].cx, data[2].cy, data[3].cx, data[3].cy); };
+                let nf_horiz_offset = 230.;
+                let nf_vert_offset = 60.;
+                let nf_mag_ratio = 3.0;
+                for point in &mut nf_points {
+                    point.x -= nf_horiz_offset;
+                    point.y -= nf_vert_offset;
+                    point.x /= nf_mag_ratio;
+                    point.y /= nf_mag_ratio;
+                    point.x = 4096./nf_mag_ratio+point.x;
+                    point.y = 4096./nf_mag_ratio+point.y;
                 }
-            }
 
-            let mut state = self.state.lock().await;
-            state.nf_aim_point = nf_aim_point;
-            state.wf_aim_point = wf_aim_point;
-            state.nf_data = Some(nf_data);
-            state.wf_data = Some(wf_data);
+                for (points, aim_point) in [(&mut nf_points, &mut nf_aim_point), (&mut wf_points, &mut wf_aim_point)] {
+                    if points.len() > 3 {
+                        sort_clockwise(&mut points[0..4]);
+                        *aim_point = transform_aim_point_to_identity(center_aim, points[0], points[1], points[2], points[3]);
+                        if aim_point.is_none() { println!("Linear resolution failed with points: ({},{}), ({},{}), ({},{}), ({},{})", points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y, points[3].x, points[3].y); };
+                    }
+                }
+
+                if nf_points.len() > 3 {
+                    fv_aim_point = nf_aim_point;
+                } else if nf_points.len() == 0 {
+                    fv_aim_point = wf_aim_point;
+                } else if wf_points.len() > 3 {
+                    let mut index_claim_map = [None::<usize>; 4];
+                    let mut dist_map = [0.; 4];
+                    for (i, wf_point) in wf_points[0..4].iter().enumerate() {
+                        for (j, nf_point) in nf_points.iter().enumerate() {
+                            let _dist = ((wf_point.x-nf_point.x).powi(2) + (wf_point.y-nf_point.y).powi(2)).sqrt();
+                            if dist_map[i] > _dist {
+                                if index_claim_map[j].is_none() || dist_map[index_claim_map[j].unwrap()] > _dist {
+                                    index_claim_map[j] = Some(i);
+                                    dist_map[i] = _dist;
+                                }
+                            }
+                        }
+                    }
+                    let mut nf_for_wf_map = [None::<Point2<f64>>; 4];
+                    for (nf_index, wf_index) in index_claim_map.iter().enumerate() {
+                        if let Some(wf_index) = wf_index {
+                            nf_for_wf_map[*wf_index] = Some(nf_points[nf_index]);
+                        }
+                    }
+                    let mut points = nf_for_wf_map.into_iter().zip(wf_points).map(
+                        |(nf_point_option, wf_point)| {
+                            if let Some(nf_point) = nf_point_option {
+                                nf_point
+                            } else {
+                                wf_point
+                            }
+                        }
+                    ).collect::<ArrayVec<Point2<f64>, 4>>();
+                    fv_aim_point = transform_aim_point_to_identity(center_aim, points[0], points[1], points[2], points[3]);
+                }
+
+                let mut state = self.state.lock().await;
+                state.fv_aim_point = fv_aim_point;
+                state.nf_aim_point = nf_aim_point;
+                state.wf_aim_point = wf_aim_point;
+                state.nf_data = Some(nf_data);
+                state.wf_data = Some(wf_data);
+            }
             sleep(Duration::from_millis(5)).await;
         }
     }
--- vision-module-gui/src/test_canvas.rs
+++ vision-module-gui/src/test_canvas.rs
@@ -17,9 +17,14 @@ impl AreaHandler for TestCanvas {
     fn draw(&mut self, _area: &Area, draw_params: &AreaDrawParams) {
         let ctx = &draw_params.context;
 
+        let fv_ch_path = Path::new(ctx, FillMode::Winding);
         let nf_ch_path = Path::new(ctx, FillMode::Winding);
         let wf_ch_path = Path::new(ctx, FillMode::Winding);
         let state = self.state.blocking_lock();
+        if let Some(aim_point) = state.fv_aim_point {
+            draw_crosshair(&ctx, &fv_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height);
+        }
+        fv_ch_path.end(ctx);
         if let Some(aim_point) = state.nf_aim_point {
             draw_crosshair(&ctx, &nf_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height);
         }
@@ -37,6 +42,15 @@ impl AreaHandler for TestCanvas {
             dashes: vec![],
             dash_phase: 0.,
         };
+
+        let brush = Brush::Solid(SolidBrush {
+            r: 1.,
+            g: 0.,
+            b: 1.,
+            a: 1.,
+        });
+
+        ctx.stroke(&fv_ch_path, &brush, &stroke);
 
         let brush = Brush::Solid(SolidBrush {
             r: 1.,
