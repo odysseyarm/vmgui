9451c5788c4bcd9cb25d282018e40c446d37e482
Abraham Hamidi <abraham.hamidi01@gmail.com>
2023-10-18 14:35:44 -0500

    Make crosshairs more visible

--- vision-module-gui/src/custom_shapes.rs
+++ vision-module-gui/src/custom_shapes.rs
@@ -1,10 +1,10 @@
 use iui::draw;
 use iui::draw::Path;
 
-pub fn draw_crosshair(ctx: &draw::DrawContext, path: &Path, x: f64, y: f64) {
-    path.new_figure(ctx, x-8., y);
-    path.line_to(ctx, x+8., y);
+pub fn draw_crosshair(ctx: &draw::DrawContext, path: &Path, x: f64, y: f64, r: f64) {
+    path.new_figure(ctx, x-r, y);
+    path.line_to(ctx, x+r, y);
 
-    path.new_figure(ctx, x, y-8.);
-    path.line_to(ctx, x, y+8.);
+    path.new_figure(ctx, x, y-r);
+    path.line_to(ctx, x, y+r);
 }
--- vision-module-gui/src/test_canvas.rs
+++ vision-module-gui/src/test_canvas.rs
@@ -22,35 +22,44 @@ impl AreaHandler for TestCanvas {
         let wf_ch_path = Path::new(ctx, FillMode::Winding);
         let state = self.state.blocking_lock();
         if let Some(aim_point) = state.fv_aim_point {
-            draw_crosshair(&ctx, &fv_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height);
+            draw_crosshair(&ctx, &fv_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height, 30.);
         }
         fv_ch_path.end(ctx);
         if let Some(aim_point) = state.nf_aim_point {
-            draw_crosshair(&ctx, &nf_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height);
+            draw_crosshair(&ctx, &nf_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height, 15.);
         }
         nf_ch_path.end(ctx);
         if let Some(aim_point) = state.wf_aim_point {
-            draw_crosshair(&ctx, &wf_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height);
+            draw_crosshair(&ctx, &wf_ch_path, aim_point.x*draw_params.area_width, aim_point.y*draw_params.area_height, 15.);
         }
         wf_ch_path.end(ctx);
 
         let stroke = StrokeParams {
             cap: 0, // Bevel
             join: 0, // Flat
-            thickness: 2.,
+            thickness: 10.,
             miter_limit: 0.,
             dashes: vec![],
             dash_phase: 0.,
         };
 
         let brush = Brush::Solid(SolidBrush {
-            r: 1.,
-            g: 0.,
-            b: 1.,
+            r: 0.,
+            g: 1.,
+            b: 0.,
             a: 1.,
         });
 
         ctx.stroke(&fv_ch_path, &brush, &stroke);
+
+        let stroke = StrokeParams {
+            cap: 0, // Bevel
+            join: 0, // Flat
+            thickness: 5.,
+            miter_limit: 0.,
+            dashes: vec![],
+            dash_phase: 0.,
+        };
 
         let brush = Brush::Solid(SolidBrush {
             r: 1.,
