6bb810e026dc2fc30a04c434839908b54b658d0c
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-01-02 18:02:13 -0600

    Add CV and CV cbindgen lib

---
+++ ats_cv_cbindgen/src/lib.rs
@@ -0,0 +1,57 @@
+#[repr(C)]
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct Pointf32 {
+    x: f32,
+    y: f32,
+}
+
+impl Pointf32 {
+    // Another associated function, taking two arguments:
+    fn new(x: f32, y: f32) -> Pointf32 {
+        Pointf32 { x, y }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn transform_aim_point_f32(aim_point: Pointf32, p1: Pointf32, p2: Pointf32, p3: Pointf32, p4: Pointf32,
+                                      np1: Pointf32, np2: Pointf32, np3: Pointf32, np4: Pointf32,
+                                      result: &mut Pointf32) -> bool {
+
+    match ats_cv::transform_aim_point(nalgebra::Point2::new(aim_point.x, aim_point.y),
+                                      nalgebra::Point2::new(p1.x, p1.y),
+                                      nalgebra::Point2::new(p2.x, p2.y),
+                                      nalgebra::Point2::new(p3.x, p3.y),
+                                      nalgebra::Point2::new(p4.x, p4.y),
+                                      nalgebra::Point2::new(np1.x, np1.y),
+                                      nalgebra::Point2::new(np2.x, np2.y),
+                                      nalgebra::Point2::new(np3.x, np3.y),
+                                      nalgebra::Point2::new(np4.x, np4.y)) {
+        None => false,
+        Some(p) => { result.x = p.x; result.y = p.y; true }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn center_aimpoint() {
+        let center_aim = Pointf32::new(0.5, 0.5);
+
+        let p1 = Pointf32::new(0.5, 1.);
+        let p2 = Pointf32::new(0., 0.5);
+        let p3 = Pointf32::new(0.5, 0.);
+        let p4 = Pointf32::new(1., 0.5);
+
+        let mut result = Pointf32::new(0., 0.);
+
+        transform_aim_point_f32(center_aim, p1, p2, p3, p4,
+                                         Pointf32::new(0.5, 1.), Pointf32::new(0., 0.5),
+                                         Pointf32::new(0.5, 0.), Pointf32::new(1., 0.5),
+                                         &mut result);
+
+        assert_eq!(result, Pointf32::new(0.5, 0.5));
+    }
+}
