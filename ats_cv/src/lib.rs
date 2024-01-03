dd92b8744a5e610405f27424473f0767a326c3de
Matthew Tran <0e4ef622@gmail.com>
2024-01-03 00:54:52 -0600

    Increase sanity in ats_cv libs

--- ats_cv/src/lib.rs
+++ ats_cv/src/lib.rs
@@ -8,14 +8,14 @@ pub fn transform_aim_point<F: Float + FromPrimitive + 'static>(aim_point: Point2
                        np1: Point2<F>, np2: Point2<F>, np3: Point2<F>, np4: Point2<F>) -> Option<Point2<F>> where F: ComplexField, F: RealField {
 
     let a = SMatrix::<F,8,8>::from_row_slice(
-        &[p1.x, p1.y, F::from_f32(1.).unwrap(), F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), -np1.x*p1.x, -np1.x*p1.y,
-            F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), p1.x, p1.y, F::from_f32(1.).unwrap(), -np1.y*p1.x, -np1.y*p1.y,
-            p2.x, p2.y, F::from_f32(1.).unwrap(), F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), -np2.x*p2.x, -np2.x*p2.y,
-            F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), p2.x, p2.y, F::from_f32(1.).unwrap(), -np2.y*p2.x, -np2.y*p2.y,
-            p3.x, p3.y, F::from_f32(1.).unwrap(), F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), -np3.x*p3.x, -np3.x*p3.y,
-            F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), p3.x, p3.y, F::from_f32(1.).unwrap(), -np3.y*p3.x, -np3.y*p3.y,
-            p4.x, p4.y, F::from_f32(1.).unwrap(), F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), -np4.x*p4.x, -np4.x*p4.y,
-            F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(),  F::from_f32(0.).unwrap(), p4.x, p4.y, F::from_f32(1.).unwrap(), -np4.y*p4.x, -np4.y*p4.y],
+        &[p1.x, p1.y, F::one(), F::zero(),  F::zero(),  F::zero(), -np1.x*p1.x, -np1.x*p1.y,
+            F::zero(),  F::zero(),  F::zero(), p1.x, p1.y, F::one(), -np1.y*p1.x, -np1.y*p1.y,
+            p2.x, p2.y, F::one(), F::zero(),  F::zero(),  F::zero(), -np2.x*p2.x, -np2.x*p2.y,
+            F::zero(),  F::zero(),  F::zero(), p2.x, p2.y, F::one(), -np2.y*p2.x, -np2.y*p2.y,
+            p3.x, p3.y, F::one(), F::zero(),  F::zero(),  F::zero(), -np3.x*p3.x, -np3.x*p3.y,
+            F::zero(),  F::zero(),  F::zero(), p3.x, p3.y, F::one(), -np3.y*p3.x, -np3.y*p3.y,
+            p4.x, p4.y, F::one(), F::zero(),  F::zero(),  F::zero(), -np4.x*p4.x, -np4.x*p4.y,
+            F::zero(),  F::zero(),  F::zero(), p4.x, p4.y, F::one(), -np4.y*p4.x, -np4.y*p4.y],
     );
     let axp = SVector::from_row_slice(&[np1.x, np1.y, np2.x, np2.y, np3.x, np3.y, np4.x, np4.y]);
     let decomp = a.lu();
@@ -23,7 +23,7 @@ pub fn transform_aim_point<F: Float + FromPrimitive + 'static>(aim_point: Point2
     let transformation = Matrix3::new(
         p[0], p[1], p[2],
         p[3], p[4], p[5],
-        p[6], p[7], F::from(1.).unwrap()
+        p[6], p[7], F::one(),
     );
     Some(transformation.transform_point(&aim_point))
 }
