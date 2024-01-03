21952e00f55190a87a5ba137f4a32e72767b00cc
Matthew Tran <0e4ef622@gmail.com>
2024-01-03 00:54:52 -0600

    Increase sanity in ats_cv libs

--- ats_cv_cbindgen/src/lib.rs
+++ ats_cv_cbindgen/src/lib.rs
@@ -1,14 +1,19 @@
-#![no_std]
+#![cfg_attr(target_os = "none", no_std)]
 
-#[cfg(not(test))]
+#[cfg(target_os = "none")]
 use panic_rtt_target as _;
 
 #[repr(C)]
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Pointf32 {
     x: f32,
     y: f32,
+}
+
+impl From<Pointf32> for nalgebra::Point2<f32> {
+    fn from(p: Pointf32) -> Self {
+        nalgebra::Point2::new(p.x, p.y)
+    }
 }
 
 #[no_mangle]
@@ -16,40 +21,16 @@ pub extern "C" fn transform_aim_point_f32(aim_point: Pointf32, p1: Pointf32, p2:
                                       np1: Pointf32, np2: Pointf32, np3: Pointf32, np4: Pointf32,
                                       result: &mut Pointf32) -> bool {
 
-    match ats_cv::transform_aim_point(nalgebra::Point2::new(aim_point.x, aim_point.y),
-                                      nalgebra::Point2::new(p1.x, p1.y),
-                                      nalgebra::Point2::new(p2.x, p2.y),
-                                      nalgebra::Point2::new(p3.x, p3.y),
-                                      nalgebra::Point2::new(p4.x, p4.y),
-                                      nalgebra::Point2::new(np1.x, np1.y),
-                                      nalgebra::Point2::new(np2.x, np2.y),
-                                      nalgebra::Point2::new(np3.x, np3.y),
-                                      nalgebra::Point2::new(np4.x, np4.y)) {
+    match ats_cv::transform_aim_point(aim_point.into(),
+                                      p1.into(),
+                                      p2.into(),
+                                      p3.into(),
+                                      p4.into(),
+                                      np1.into(),
+                                      np2.into(),
+                                      np3.into(),
+                                      np4.into()) {
         None => false,
         Some(p) => { result.x = p.x; result.y = p.y; true }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn center_aimpoint() {
-        let center_aim = Pointf32 { x: 0.5, y: 0.5 };
-
-        let p1 = Pointf32 { x: 0.5, y: 1. };
-        let p2 = Pointf32 { x: 0., y: 0.5 };
-        let p3 = Pointf32 { x: 0.5, y: 0. };
-        let p4 = Pointf32 { x: 1., y: 0.5 };
-
-        let mut result = Pointf32 { x: 0., y: 0. };
-
-        transform_aim_point_f32(center_aim, p1, p2, p3, p4,
-                                         Pointf32 { x: 0.5, y: 1.}, Pointf32 { x: 0., y: 0.5},
-                                         Pointf32 { x: 0.5, y: 0. }, Pointf32 { x: 1., y: 0.5 },
-                                         &mut result);
-
-        assert_eq!(result, Pointf32 { x: 0.5, y: 0.5 });
     }
 }
