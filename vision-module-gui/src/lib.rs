7e5e864879d230dd3932cecc26355a99745dcf1b
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-03-19 01:28:23 -0500

    Separate canvas for combined markers and undistort widefield

--- vision-module-gui/src/lib.rs
+++ vision-module-gui/src/lib.rs
@@ -2,6 +2,7 @@ use ahrs::Madgwick;
 use arrayvec::ArrayVec;
 use nalgebra::{Matrix3x1, Point2, SMatrix, Matrix3};
 use ats_cv::kalman::Pva2d;
+use opencv_ros_camera::{Distortion, RosOpenCvIntrinsics};
 use serde::Serialize;
 use crate::packet::MotData;
 
@@ -11,6 +12,7 @@ pub mod config_window;
 pub mod marker_config_window;
 pub mod layout_macro;
 pub mod mot_runner;
+pub mod run_raw_canvas;
 pub mod run_canvas;
 pub mod test_canvas;
 pub mod custom_shapes;
@@ -40,11 +42,19 @@ pub struct MotState {
     pub wf_data: Option<ArrayVec<MotData, 16>>,
     pub nf_pva2ds: [Pva2d<f64>; 4],
 
+    nf_points: ArrayVec<Point2<f64>, 16>,
+    wf_points: ArrayVec<Point2<f64>, 16>,
+
+    nf_radii: [u8; 4],
+    wf_radii: [u8; 4],
+
     pub screen_id: u8,
     pub orientation: Madgwick<f64>,
 
     pub rotation_mat: Matrix3<f64>,
     pub translation_mat: Matrix3x1<f64>,
+
+    pub camera_model: RosOpenCvIntrinsics<f64>,
 }
 
 impl Default for MotState {
@@ -60,6 +70,24 @@ impl Default for MotState {
             orientation: Madgwick::new(1./800., 0.1),
             rotation_mat: Default::default(),
             translation_mat: Default::default(),
+            camera_model: RosOpenCvIntrinsics::from_params_with_distortion(
+                6070.516352962917,     // fx 
+                0.0,
+                6081.704092101642,     // fy 
+                2012.5340172306787,    // cx 
+                2053.6576652187186,    // cy 
+                Distortion::from_opencv_vec(nalgebra::Vector5::new(
+                    -0.08717430574570494,  // k1 
+                    0.5130932472490415,    // k2 
+                    0.0050450411569348645, // p1 
+                    0.001854950091801636,  // p2 
+                    0.048480928208383456,  // k3 
+                )),
+            ),
+            nf_points: Default::default(),
+            wf_points: Default::default(),
+            nf_radii: Default::default(),
+            wf_radii: Default::default(),
         }
     }
 }
