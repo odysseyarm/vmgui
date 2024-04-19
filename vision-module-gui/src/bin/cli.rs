82d05f91aecb463aaae090e3d46a144411d2acb5
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-04-19 15:06:14 -0500

    Await before unwrap connect hub

--- vision-module-gui/src/bin/cli.rs
+++ vision-module-gui/src/bin/cli.rs
@@ -21,7 +21,7 @@ async fn main() -> ExitCode {
     let device = match &*args[1] {
         "-s" => UsbDevice::connect_serial(&args[2]).await.unwrap(),
         "-t" => UsbDevice::connect_tcp(&args[2]).unwrap(),
-        "-u" => UsbDevice::connect_hub("0.0.0.0:23456", &args[2]).unwrap(),
+        "-u" => UsbDevice::connect_hub("0.0.0.0:0", &args[2]).await.unwrap(),
         _ => {
             print_help();
             return ExitCode::FAILURE;
