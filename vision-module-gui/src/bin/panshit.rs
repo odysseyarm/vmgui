9a412f80eb0502ecfb191711ff468f0fc29bb2ec
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-04-20 13:20:21 -0500

    Timestamps

--- vision-module-gui/src/bin/panshit.rs
+++ vision-module-gui/src/bin/panshit.rs
@@ -5,7 +5,7 @@ use ats_usb::device::{decode_slip_frame, SLIP_FRAME_END};
 fn main() {
     let client = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 23456)).unwrap();
     client.set_broadcast(true).unwrap();
-    client.send_to(&[255, 1], ("192.168.68.255", 23456)).unwrap();
+    client.send_to(&[255, 1], ("10.0.0.255", 23456)).unwrap();
 
     let mut udp_packet = vec![1, 0, 255];
     let pkt = Packet {
@@ -13,7 +13,7 @@ fn main() {
         data: PacketData::ReadRegister(Register { port: Port::Wf, bank: 0x00, address: 0x02 }),
     };
     pkt.serialize(&mut udp_packet);
-    client.send_to(&udp_packet, ("192.168.68.255", 23456)).unwrap();
+    client.send_to(&udp_packet, ("10.0.0.255", 23456)).unwrap();
 
     let mut udp_packet = vec![1, 0, 255];
     let pkt = Packet {
