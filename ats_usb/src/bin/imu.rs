09ac887a491426d2ccad1419e8c0245f5e3c8204
Matthew Tran <0e4ef622@gmail.com>
2024-06-03 23:41:32 -0500

    Add simple program to log imu data

---
+++ ats_usb/src/bin/imu.rs
@@ -0,0 +1,35 @@
+#![cfg(feature = "bin")]
+
+use nalgebra::Vector3;
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+    let device = ats_usb::device::UsbDevice::connect_serial(path, false).await.unwrap();
+    let mut s = device.stream_accel().await.unwrap();
+
+    let mut accel_sum = Vector3::zeros();
+    let mut gyro_sum = Vector3::zeros();
+    let mut count = 0.0;
+
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => {},
+        _ = async {
+            loop {
+                let v = s.next().await.unwrap();
+                println!("accel = {:8.4?}, gyro = {:8.4?}", v.accel, v.gyro);
+                count += 1.0;
+                accel_sum += v.accel;
+                gyro_sum += v.gyro;
+            }
+        }=> {},
+    }
+
+    println!();
+    println!("Mean accel: {:.8?}", accel_sum / count);
+    println!("Mean accel magnitude: {:.8?}", (accel_sum / count).magnitude());
+    println!("Mean gyro: {:.8?}", gyro_sum / count);
+    println!("Samples: {count}");
+}
