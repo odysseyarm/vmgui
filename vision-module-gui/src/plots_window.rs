656bea3ece00141dd3ce2426b5fff466462f8182
Matthew Tran <0e4ef622@gmail.com>
2024-06-08 01:20:00 -0500

    Add labels to plots and pnp orientation

--- vision-module-gui/src/plots_window.rs
+++ vision-module-gui/src/plots_window.rs
@@ -1,7 +1,16 @@
-use std::error::Error;
-
-use iui::{controls::{Area, AreaDrawParams, AreaHandler, Window, WindowType}, draw::plotters::PlottersBackend, UI};
-use plotters::{backend::DrawingBackend, chart::ChartBuilder, coord::{CoordTranslate, Shift}, drawing::{DrawingArea, IntoDrawingArea}, element::Rectangle, series::LineSeries, style::{Color, HSLColor, BLUE, GREEN, RED, WHITE}};
+use iui::{
+    controls::{Area, AreaDrawParams, AreaHandler, Window, WindowType},
+    draw::plotters::PlottersBackend,
+    UI,
+};
+use plotters::{
+    backend::DrawingBackend,
+    chart::{ChartBuilder, LabelAreaPosition},
+    coord::Shift,
+    drawing::{DrawingArea, IntoDrawingArea},
+    series::LineSeries,
+    style::{BLUE, GREEN, RED, WHITE},
+};
 
 use crate::CloneButShorter;
 
@@ -40,7 +49,7 @@ struct MainCanvas;
 
 impl AreaHandler for MainCanvas {
     fn draw(&mut self, _area: &Area, draw_params: &AreaDrawParams) {
-        let mut root = PlottersBackend::new(
+        let root = PlottersBackend::new(
             draw_params,
             (
                 draw_params.area_width as u32,
@@ -48,67 +57,104 @@ impl AreaHandler for MainCanvas {
             ),
         )
         .into_drawing_area();
-        root.fill(&WHITE);
+        root.fill(&WHITE).unwrap();
         let subplots = root.split_evenly((2, 2));
 
         gyro_chart(&subplots[0]);
         accel_chart(&subplots[1]);
         pnp_position_chart(&subplots[2]);
+        pnp_orientation_chart(&subplots[3]);
     }
 }
 
-fn gyro_chart<DB: DrawingBackend>(
-    area: &DrawingArea<DB, Shift>,
-) {
+fn gyro_chart<DB: DrawingBackend>(area: &DrawingArea<DB, Shift>) {
     let series = ats_cv::telemetry::imu_data();
     let mut chart = ChartBuilder::on(area)
         .caption("Angular Velocity", ("sans-serif", 12))
+        .margin(20)
+        .set_left_and_bottom_label_area_size(30)
         .build_cartesian_2d(0..series.size, -20.0..20.0)
         .unwrap();
+    chart.configure_mesh().draw().unwrap();
 
     let data = series.values.lock().unwrap();
 
-    let x = data.iter().map(|p| p.1.1.x).enumerate();
-    let y = data.iter().map(|p| p.1.1.y).enumerate();
-    let z = data.iter().map(|p| p.1.1.z).enumerate();
+    let x = data.iter().map(|p| p.1 .1.x).enumerate();
+    let y = data.iter().map(|p| p.1 .1.y).enumerate();
+    let z = data.iter().map(|p| p.1 .1.z).enumerate();
     chart.draw_series(LineSeries::new(x, &RED)).unwrap();
     chart.draw_series(LineSeries::new(y, &GREEN)).unwrap();
     chart.draw_series(LineSeries::new(z, &BLUE)).unwrap();
 }
 
-fn accel_chart<DB: DrawingBackend>(
-    area: &DrawingArea<DB, Shift>,
-) {
+fn accel_chart<DB: DrawingBackend>(area: &DrawingArea<DB, Shift>) {
     let series = ats_cv::telemetry::imu_data();
     let mut chart = ChartBuilder::on(area)
         .caption("Acceleration", ("sans-serif", 12))
+        .margin(20)
+        .set_left_and_bottom_label_area_size(30)
         .build_cartesian_2d(0..series.size, -30.0..30.0)
+        .unwrap();
+    chart
+        .configure_mesh()
+        .set_tick_mark_size(LabelAreaPosition::Left, 5)
+        .draw()
         .unwrap();
 
     let data = series.values.lock().unwrap();
 
-    let x = data.iter().map(|p| p.1.0.x).enumerate();
-    let y = data.iter().map(|p| p.1.0.y).enumerate();
-    let z = data.iter().map(|p| p.1.0.z).enumerate();
+    let x = data.iter().map(|p| p.1 .0.x).enumerate();
+    let y = data.iter().map(|p| p.1 .0.y).enumerate();
+    let z = data.iter().map(|p| p.1 .0.z).enumerate();
     chart.draw_series(LineSeries::new(x, &RED)).unwrap();
     chart.draw_series(LineSeries::new(y, &GREEN)).unwrap();
     chart.draw_series(LineSeries::new(z, &BLUE)).unwrap();
 }
 
-fn pnp_position_chart<DB: DrawingBackend>(
-    area: &DrawingArea<DB, Shift>,
-) {
+fn pnp_position_chart<DB: DrawingBackend>(area: &DrawingArea<DB, Shift>) {
     let series = ats_cv::telemetry::pnp_solutions();
     let mut chart = ChartBuilder::on(area)
         .caption("PnP Position", ("sans-serif", 12))
-        .build_cartesian_2d(0..series.size, -30.0..30.0)
+        .margin(20)
+        .set_left_and_bottom_label_area_size(30)
+        .build_cartesian_2d(0..series.size, -10.0..10.0)
         .unwrap();
+    chart.configure_mesh().draw().unwrap();
 
     let data = series.values.lock().unwrap();
 
     let x = data.iter().map(|p| p.1.translation.x).enumerate();
     let y = data.iter().map(|p| p.1.translation.y).enumerate();
     let z = data.iter().map(|p| p.1.translation.z).enumerate();
+    chart.draw_series(LineSeries::new(x, &RED)).unwrap();
+    chart.draw_series(LineSeries::new(y, &GREEN)).unwrap();
+    chart.draw_series(LineSeries::new(z, &BLUE)).unwrap();
+}
+
+fn pnp_orientation_chart<DB: DrawingBackend>(area: &DrawingArea<DB, Shift>) {
+    let series = ats_cv::telemetry::pnp_solutions();
+    let mut chart = ChartBuilder::on(area)
+        .caption("PnP Orientation", ("sans-serif", 12))
+        .margin(20)
+        .set_left_and_bottom_label_area_size(30)
+        .build_cartesian_2d(0..series.size, -3.15..3.15)
+        .unwrap();
+    chart.configure_mesh().draw().unwrap();
+
+    let data = series.values.lock().unwrap();
+
+    let x = data
+        .iter()
+        .map(|p| p.1.rotation.euler_angles().0)
+        .enumerate();
+    let y = data
+        .iter()
+        .map(|p| p.1.rotation.euler_angles().1)
+        .enumerate();
+    let z = data
+        .iter()
+        .map(|p| p.1.rotation.euler_angles().2)
+        .enumerate();
     chart.draw_series(LineSeries::new(x, &RED)).unwrap();
     chart.draw_series(LineSeries::new(y, &GREEN)).unwrap();
     chart.draw_series(LineSeries::new(z, &BLUE)).unwrap();
