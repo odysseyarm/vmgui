ff7a501f0db7781263177935af81a1d79c4aeb65
Matthew Tran <0e4ef622@gmail.com>
2024-07-25 17:41:15 -0500

    Also calculate accel scaling

--- ats_usb/cli/accel_bias_calc.py
+++ ats_usb/cli/accel_bias_calc.py
@@ -1,22 +1,24 @@
 from sympy import Symbol, nsolve
-x_x, x_y, x_z, \
-y_x, y_y, y_z, \
-z_x, z_y, z_z, \
-b_x, b_y, b_z, \
-g = map(Symbol, "x_x x_y x_z y_x y_y y_z z_x z_y z_z b_x b_y b_z g".split())
+b_x, b_y, b_z, s_x, s_y, s_z, g = map(Symbol, "b_x b_y b_z s_x s_y s_z g".split())
 
 
-# 3 accel measurements
-x_x, x_y, x_z = [0.29391649, 0.80576897, 9.93242264]
-y_x, y_y, y_z = [9.50738144, -0.17829479, -2.44243479]
-z_x, z_y, z_z = [-1.26677823, 9.09933949, 3.32616949]
+# 6 accel measurements
+x_x, x_y, x_z = [0.13028911, 0.12664859, 9.98692131]
+y_x, y_y, y_z = [-9.50260544, 0.07374427, -2.35186720]
+z_x, z_y, z_z = [0.17701271, 7.17228413, -6.64125538]
+a_x, a_y, a_z = [8.29341698, 4.16669178, -3.09963512]
+c_x, c_y, c_z = [3.54227853, 5.16701508, 7.60187197]
+d_x, d_y, d_z = [2.97791672, -2.96459055, 9.08387566]
 
 # Gravity in Houston determined by https://www.ngs.noaa.gov/cgi-bin/grav_pdx.prl
 g = 9.79281
 
-eq1 = (x_x + b_x)**2 + (x_y + b_y)**2 + (x_z + b_z)**2 - g**2
-eq2 = (y_x + b_x)**2 + (y_y + b_y)**2 + (y_z + b_z)**2 - g**2
-eq3 = (z_x + b_x)**2 + (z_y + b_y)**2 + (z_z + b_z)**2 - g**2
+eq1 = (s_x*x_x + b_x)**2 + (s_y*x_y + b_y)**2 + (s_z*x_z + b_z)**2 - g**2
+eq2 = (s_x*y_x + b_x)**2 + (s_y*y_y + b_y)**2 + (s_z*y_z + b_z)**2 - g**2
+eq3 = (s_x*z_x + b_x)**2 + (s_y*z_y + b_y)**2 + (s_z*z_z + b_z)**2 - g**2
+eq4 = (s_x*a_x + b_x)**2 + (s_y*a_y + b_y)**2 + (s_z*a_z + b_z)**2 - g**2
+eq5 = (s_x*c_x + b_x)**2 + (s_y*c_y + b_y)**2 + (s_z*c_z + b_z)**2 - g**2
+eq6 = (s_x*d_x + b_x)**2 + (s_y*d_y + b_y)**2 + (s_z*d_z + b_z)**2 - g**2
 
-s = nsolve([eq1, eq2, eq3], [b_x, b_y, b_z], [0, 0, 0], dict=True)
+s = nsolve([eq1, eq2, eq3, eq4, eq5, eq6], [b_x, b_y, b_z, s_x, s_y, s_z], [0, 0, 0, 1, 1, 1], dict=True)
 print(s)
